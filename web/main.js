aeb0b38c06a54813ad0efce647bec516d6c17f9e
nijova <niko.jovan@protonmail.com>
2022-02-14 15:43:31 -0500

    refactor

---
+++ web/main.js
@@ -0,0 +1,7 @@
+requirejs(['root'], function(required) {
+  console.log(required)
+
+  const root = document.querySelector('#root');
+  ReactDOM.render(React.createElement(Root), root);
+
+});
---
+++ web/root.js
@@ -0,0 +1,11 @@
+class Root extends React.Component {  
+  render() {
+    return React.createElement(
+      'h1',
+      {},
+      'YugabyteDB ...coming soon'
+    );
+  }
+}
+
+define(new Root());
