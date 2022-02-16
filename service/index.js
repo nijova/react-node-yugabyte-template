83669fc1223ce657da02ded2f55ceddfe872b614
nijova <niko.jovan@protonmail.com>
2022-02-16 16:33:52 -0500

    build api

--- service/index.js
+++ service/index.js
@@ -5,8 +5,20 @@ const app = express();
 app.use(cors());
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/:id', (req, res) => {
+  res.send('get this id: ' + req.params.id);
+})
+
+app.post('/:id', (req, res) => {
+  res.send('post this id: ' + req.params.id);
+})
+
+app.patch('/:id', (req, res) => {
+  res.send('patch this id: ' + req.params.id);
+})
+
+app.delete('/:id', (req, res) => {
+  res.send('delete this id: ' + req.params.id);
 })
 
 app.listen(port, () => {
